58a6884ff4c46ae485767cdf05dcc69e8fd9bc7f
adityaraj-09 <itsrishabhforyou@gmail.com>
2023-07-02 13:05:06 +0530

    commit

--- ProductApi.js
+++ ProductApi.js
@@ -25,4 +25,16 @@ router.post("/api/addProduct",async (req,res) =>{
 
 });
 
+router.get("/api/products",async (req,res)=>{
+ try{
+ const products=await Product.find();
+ res.json(products);
+ }
+ catch (error){
+ res.status(500).json({error:error.message});
+ }
+});
+
+
+
 module.exports=router;
